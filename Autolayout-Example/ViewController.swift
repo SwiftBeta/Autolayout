9bb40b3572661e8e592a60676e21e3563b92d926
SwiftBeta <swiftbeta.blog@gmail.com>
2020-11-22 19:49:46 +0100

    Added setContentHuggingPriority and setContentCompressionResistancePriority

--- Autolayout-Example/ViewController.swift
+++ Autolayout-Example/ViewController.swift
@@ -34,6 +34,31 @@ class ViewController: UIViewController {
         return view
     }()
     
+    let descriptionLeftLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Aprende a programar"
+        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionRightLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Swift desde cero"
+        label.backgroundColor = .yellow
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let clickButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Tap me please!", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,11 +81,47 @@ class ViewController: UIViewController {
             containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            
         ])
         
 //        nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 //        nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        addDescriptions()
+        addButton()
+    }
+    
+    private func addDescriptions() {
+        containerView.addSubview(descriptionLeftLabel)
+        containerView.addSubview(descriptionRightLabel)
+        
+        NSLayoutConstraint.activate([
+            descriptionLeftLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
+            descriptionLeftLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
+            
+            
+            descriptionLeftLabel.trailingAnchor.constraint(equalTo: descriptionRightLabel.leadingAnchor),
+            
+            descriptionRightLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
+            descriptionRightLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
+        ])
+        
+        descriptionLeftLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        descriptionRightLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    private func addButton() {
+        containerView.addSubview(clickButton)
+        
+        let widthConstaint = clickButton.widthAnchor.constraint(equalToConstant: 60)
+        
+        NSLayoutConstraint.activate([
+            clickButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            clickButton.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            widthConstaint
+        ])
+        
+        widthConstaint.priority = .defaultLow
+        clickButton.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
     }
 }
 
